479ea29ab98c8ec8493d57eb1ff56ebff36e0c92
Perry Zheng <pzheng@twitter.com>
2014-08-28 23:44:12 -0700

    get the tip to be in the table view.
    there is some problem with not releasing the cell. (the first couple lines get populated with 10 people)

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -15,11 +15,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var checkSplitTableView: UITableView!;
     
     @IBOutlet weak var billField: UITextField!
-    @IBOutlet weak var tipLabel: UILabel!
+    //@IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    let uiImage = UIImage(named:"person.png")
+    let numPeople: Int = 10
     var items: [String] = ["hi", "foo", "bar"]
+    private var total: Double = 0.0
     //@IBOutlet weak var totalAmountsView: UIView!
     
     override func viewDidLoad() {
@@ -27,6 +30,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view, typically from a nib.
         billField.becomeFirstResponder()
         billField.text = rawAmount
+        onEditingChanged(self)
     }
 
     @IBAction func onTap(sender: AnyObject) {
@@ -39,21 +43,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
+        checkSplitTableView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = tableView.dequeueReusableCellWithIdentifier("person split cell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.textAlignment = NSTextAlignment.Right
+        cell.textLabel.textColor = UIColor.greenColor()
+
         var tipPercentages = [0.18, 0.20, 0.22]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
         var billAmount = billField.text._bridgeToObjectiveC().doubleValue
         var tip = billAmount * tipPercentage
-        var total = billAmount + tip
-
-        tipLabel.text = String(format: "$%.2f", tip)
-        //totalLabel.text = String(format: "$%.2f", total)
+        total = billAmount + tip
+        
+        if (indexPath.row == 0)
+        {
+            cell.textLabel.text = String(format: "$%.2f", tip)
+            cell.textLabel.font = UIFont(name:"HelveticaNeue-Light", size: 20.0)
+        }
+        else
+        {
+            for var i = 0; i < indexPath.row; i++
+            {
+                cell.contentView.addSubview(getPersonImageView(i))
+            }
+            cell.textLabel.text = String(format: "$%.2f", total / Double(indexPath.row + 1))
+            cell.textLabel.font = UIFont(name:"HelveticaNeue-Light", size: 40.0)
+        }
+        return cell
     }
     
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell = tableView.dequeueReusableCellWithIdentifier("person split cell", forIndexPath: indexPath) as UITableViewCell
-        cell.textLabel.text = items[indexPath.row]
-        return cell
+    func getPersonImageView(personI: Int) -> UIImageView! {
+        let x: Double = Double(personI) * 20.0
+        var imv = UIImageView(frame: CGRectMake(CGFloat(x), 7.8, 40, 40));
+        imv.image = uiImage
+        return imv
     }
     
     func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
@@ -61,7 +87,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return numPeople+1
     }
 }
 
