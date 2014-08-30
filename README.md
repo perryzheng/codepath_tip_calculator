8eac0109311d055d04ecff920952bff97e1edb2c
Perry Zheng <pzheng@twitter.com>
2014-08-29 23:40:42 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Tips app for codepath
+
+This is a simple tips app. 
+
+Optionaly features of iOS I used
+1. Conditionally segueing
+2. Loading an image
+3. UITableView within a UIView
---
+++ tips/Model/TipsNSUserDefaults.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentAmount.swift
+//  tips
+//
+//  Created by Perry Zheng on 8/29/14.
+//  Copyright (c) 2014 Perry Zheng. All rights reserved.
+//
+
+import UIKit
+
+class CurrentAmount: NSObject {
+   
+}
